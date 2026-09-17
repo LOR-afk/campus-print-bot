8b2bcc01e99d8fbee712222670064a1ce339079e
loretams <lobe.tamayo.coc@phinmaed.com>
2026-09-18 00:07:52 +0800

    Fix production maintenance driver

--- config/app.php
+++ config/app.php
@@ -118,9 +118,9 @@ return [
     |
     */
 
-    'maintenance' => [
-        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
-        'store' => env('APP_MAINTENANCE_STORE', 'database'),
-    ],
+'maintenance' => [
+    'driver' => env('APP_MAINTENANCE_DRIVER') ?: 'file',
+    'store' => env('APP_MAINTENANCE_STORE') ?: 'database',
+],
 
 ];
